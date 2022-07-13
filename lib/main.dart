fb6e9bc7e4e138edaf28d939ba766b30e28feb86
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-07-13 20:06:17 +0300

    added search icon and fixed bad with text

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,27 @@ void main() {
       title: 'WarmBaby',
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.black54,
-          title: const Text('Warm Baby'),
+          backgroundColor: Colors.black,
+          title: const Text('W.a.r.m Baby'),
           centerTitle: true,
         ),
-        body: const Center(
-          child: Text(
-            'children clothes',
-            style: TextStyle(fontSize: 16, fontFamily: 'Montserrat'),
-          ),
+        body: Row(
+          children: [
+            IconButton(
+              icon: const Icon(
+                Icons.search,
+                size: 34,
+              ),
+              onPressed: () {
+                print('поиск');
+              },
+            ),
+            const Text(
+              'Search',
+              style: TextStyle(
+                  fontSize: 24, fontFamily: 'Montserrat', color: Colors.black),
+            ),
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.black12,
